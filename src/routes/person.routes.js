905b9fdcf48b6b6049da0240fb3a876387927cdd
Apoorva, Teja (av341a) <av341a@att.com>
2018-05-22 17:25:13 +0530

    get api

--- src/routes/person.routes.js
+++ src/routes/person.routes.js
@@ -1,11 +1,24 @@
 const express = require('express');
 
 const personRoutes = express.Router();
+const PersonModel = require('../model/person.model');
 
 
 personRoutes.get('/test', (req, res) => {
 
+    var person=new personRoutes();
+    
     res.send('test person routes');
 });
+
+
+personRoutes.get('/getAllPerson', (req, res) => {
+    PersonModel.find({}).lean().exec((error, result)=>{
+        res.send(result);
+    });
+});
+
+
+
 
 module.exports = personRoutes;
