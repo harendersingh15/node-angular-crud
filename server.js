7731798cab617c9873512dd8e314390b9df26da2
Sivaraman Karthikeyan <sivkar77@in.ibm.com>
2018-05-22 16:12:05 +0530

    change the mongo connection and node conneciton

--- server.js
+++ server.js
@@ -2,10 +2,6 @@ const mongoose = require('mongoose');
 const express = require('express');
 
 const mongoDB = 'mongodb://127.0.0.1/personDB';
-mongoose.connect(mongoDB, () => {
-
-
-});
 
 
 
@@ -14,13 +10,12 @@ const app = express();
 const portNumber = 8081;
 
 
-
-
-
 app.get('/test', (req, res)=>{
     res.send('hi');
 })
 
-app.listen(portNumber, ()=>{
-    console.log(`server is running on ${portNumber}`);
+mongoose.connect(mongoDB, () => {
+    app.listen(portNumber, ()=>{
+        console.log(`server is running on ${portNumber}`);
+    });
 });
